054129ec44793a1ac785f1bbab59411c4b9bf0ce
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-08-09 20:21:06 +0000

    comentarios

--- quote/main.go
+++ quote/main.go
@@ -27,3 +27,44 @@ var usage = `Usage:
   quote -v | -version
   quote <market> [-output=<outputFile>]
   quote [-years=<years>|(-start=<datestr> [-end=<datestr>])] [options] [-infile=<filename>|<symbol> ...]
+
+Options:
+  -h -help             show help
+  -v -version          show version
+  -years=<years>       number of years to download [default=5]
+  -start=<datestr>     yyyy[-[mm-[dd]]]
+  -end=<datestr>       yyyy[-[mm-[dd]]] [default=today]
+  -infile=<filename>   list of symbols to download
+  -outfile=<filename>  output filename
+  -period=<period>     1m|3m|5m|15m|30m|1h|2h|4h|6h|8h|12h|d|3d|w|m [default=d]
+  -source=<source>     yahoo|tiingo|tiingo-crypto|coinbase|bittrex|binance [default=yahoo]
+  -token=<tiingo_tok>  tingo api token [default=TIINGO_API_TOKEN]
+  -format=<format>     (csv|json|hs|ami) [default=csv]
+  -adjust=<bool>       adjust yahoo prices [default=true]
+  -all=<bool>          all in one file (true|false) [default=false]
+  -log=<dest>          filename|stdout|stderr|discard [default=stdout]
+  -delay=<ms>          delay in milliseconds between quote requests
+
+Note: not all periods work with all sources
+
+Valid markets:
+etfs:       etf
+crypto:     bittrex-btc,bittrex-eth,bittrex-usdt,
+            binance-bnb,binance-btc,binance-eth,binance-usdt,
+            coinbase
+`
+
+const (
+	version    = "0.2"
+	dateFormat = "2006-01-02"
+)
+
+type quoteflags struct {
+	years   int
+	delay   int
+	start   string
+	end     string
+	period  string
+	source  string
+	token   string
+	infile  string
