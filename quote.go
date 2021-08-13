3cd70a9382e891c63a51cb2de9d7ea077fdc8ab8
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-08-13 18:05:28 +0000

    Revert "updates"

--- quote.go
+++ quote.go
@@ -1,4 +1,5 @@
 /*
 Package quote is free quote downloader library and cli
 
-Downloads daily/weekly/monthly historical price quote
+Downloads daily/weekly/monthly historical price quotes from Yahoo
+and daily/intraday data fro
