a2407c3aa744afbd476d5960c249da28c87c0dc1
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-08-14 12:33:27 +0000

    Create bootstrap

--- quote.go
+++ quote.go
@@ -2,4 +2,6 @@
 Package quote is free quote downloader library and cli
 
 Downloads daily/weekly/monthly historical price quotes from Yahoo
-and daily/intraday data fro
+and daily/intraday data from Tiingo/Bittrex/Binance
+
+Copyright 2019 Mark Chenow
