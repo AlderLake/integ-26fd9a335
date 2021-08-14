e67f33d43e08d843d452e4585349f0b447f6b172
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-08-14 23:10:56 +0000

    fix date bug

--- quote.go
+++ quote.go
@@ -4,4 +4,5 @@ Package quote is free quote downloader library and cli
 Downloads daily/weekly/monthly historical price quotes from Yahoo
 and daily/intraday data from Tiingo/Bittrex/Binance
 
-Copyright 2019 Mark Chenow
+Copyright 2019 Mark Chenoweth
+Licensed under terms of MIT license (see
