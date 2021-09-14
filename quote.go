443422120e26980c4c670025f232c671097e8f0d
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-09-14 22:28:28 +0000

    Version 2.0.9

--- quote.go
+++ quote.go
@@ -157,4 +157,4 @@ func (q Quote) Highstock() string {
 			comma = ""
 		}
 		str := fmt.Sprintf("[%d,%.*f,%.*f,%.*f,%.*f,%.*f]%s\n",
-			q.Date[bar].UnixNano()/1000000, precision, q.Open[bar], precision, q.H
+			q.Date[bar].UnixNano()/1000000, precision, q.Open[bar], precision, q.High[bar], precision, q.Low[bar], precision, q.
