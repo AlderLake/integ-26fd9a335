46615ec7e58fe651aad665d2e651ab463f74c242
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-10-27 21:26:19 +0000

    Convert specs to RSpec 3.1.7 syntax with Transpec

--- quote.go
+++ quote.go
@@ -336,4 +336,4 @@ func (q Quotes) CSV() string {
 		precision := getPrecision(quote.Symbol)
 		for bar := range quote.Close {
 			str := fmt.Sprintf("%s,%s,%.*f,%.*f,%.*f,%.*f,%.*f\n",
-				quote.Symbol, quote.Date[bar].Format("2006-01-02 15:04"), precision, quote.Open[b
+				quote.Symbol, quote.Date[bar].Format("2006-01-02 15:04"), precision, quote.Open[bar], precision, quote.High[bar], precis
