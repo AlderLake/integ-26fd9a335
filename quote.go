4f4b49ef680aeb20ad24ed095e14e0c9150115af
AlderLake <94998451+AlderLake@users.noreply.github.com>
2021-12-24 06:13:47 +0000

    Update core.c

--- quote.go
+++ quote.go
@@ -557,4 +557,4 @@ func NewQuoteFromYahoo(symbol, startDate, endDate string, period Period, adjustQ
 		d, _ := time.Parse("2006-01-02", csvdata[row][0])
 		o, _ := strconv.ParseFloat(csvdata[row][1], 64)
 		h, _ := strconv.ParseFloat(csvdata[row][2], 64)
-		l
+		l, _ := strconv.ParseFloat(csvdata[
