b6a34b866d83609cf3f16918f3b20a47eac131c2
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-01-01 13:42:26 +0000

    Update playlist.js

--- quote.go
+++ quote.go
@@ -592,4 +592,6 @@ func NewQuoteFromYahoo(symbol, startDate, endDate string, period Period, adjustQ
 	to := ParseDateString(endDate)
 
 	url := fmt.Sprintf(
-		"http://ichart.yahoo.com/table.csv?s=%s&a=%d&b=%d&c=%d&d=%d&e=%d&f=%d&g=%s&ignore=
+		"http://ichart.yahoo.com/table.csv?s=%s&a=%d&b=%d&c=%d&d=%d&e=%d&f=%d&g=%s&ignore=.csv",
+		symbol,
+		from.Month()-1, from.Da
