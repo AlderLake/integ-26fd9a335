97b1bd5309cba1ca480ef3af4cec37e0ca2d07a7
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-03-28 20:45:17 +0000

    Core/Script: Clean-Up in Scripts.

--- quote.go
+++ quote.go
@@ -792,4 +792,5 @@ func tiingoCrypto(symbol string, from, to time.Time, period Period, token string
 	type cryptoData struct {
 		Ticker        string      `json:"ticker"`
 		BaseCurrency  string      `json:"baseCurrency"`
-		QuoteCurrency string      `json:"quoteCurren
+		QuoteCurrency string      `json:"quoteCurrency"`
+		PriceData     []priceData 
