aad1b7caa345f1510e98d309be2376def16fc972
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-03-29 09:13:51 +0000

    Add SECURITY.md

--- quote.go
+++ quote.go
@@ -799,4 +799,4 @@ func tiingoCrypto(symbol string, from, to time.Time, period Period, token string
 	var crypto []cryptoData
 
 	url := fmt.Sprintf(
-		"https:
+		"https://api.tiingo.com/tiingo/crypto/prices?tickers=%
