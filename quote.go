0aea89c66ba10669da81bf1b749e566ee05486e6
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-04-03 06:05:27 +0000

    USB: xhci: don't start a halted endpoint before its new dequeue is set

--- quote.go
+++ quote.go
@@ -819,4 +819,5 @@ func tiingoCrypto(symbol string, from, to time.Time, period Period, token string
 	contents, _ := ioutil.ReadAll(resp.Body)
 	err = json.Unmarshal(contents, &crypto)
 	if err != nil {
-		Log.Printf("tiingo crypto sy
+		Log.Printf("tiingo crypto symbol '%s' error: %v\n", symbol, err)
+		re
