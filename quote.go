4af1b6e8d95e10e57c3f1471a0b5efd9d1c24152
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-04-13 15:00:30 +0000

    fix source

--- quote.go
+++ quote.go
@@ -861,4 +861,4 @@ func NewQuoteFromTiingoCrypto(symbol, startDate, endDate string, period Period, 
 }
 
 // NewQuotesFromTiingoSyms - create a list of prices from symbols in string array
-func NewQuotesFromTiin
+func NewQuotesFromTiingoSyms(symbols []string, startDate, endDate
