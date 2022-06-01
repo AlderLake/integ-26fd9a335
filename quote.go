000674fd3445decef0fc703c9450d12a547ba8b2
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-06-01 14:50:39 +0000

    Update build config

--- quote.go
+++ quote.go
@@ -1101,4 +1101,5 @@ func NewQuoteFromBittrex(symbol string, period Period) (Quote, error) {
 
 	for bar := 0; bar < numrows; bar++ {
 		q.Date[bar], _ = time.Parse("2006-01-02T15:04:05", result.OHLC[bar].T) //"2017-11-28T16:50:00"
-		q.Open[bar] = res
+		q.Open[bar] = result.OHLC[bar].O
+		q.High[bar] = result.OH
