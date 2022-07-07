809c417801559b8649efc1ac00e2c11230671904
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-07-07 19:14:49 +0000

    Missing part.

--- quote.go
+++ quote.go
@@ -1267,3 +1267,4 @@ func NewQuoteFromBinance(symbol string, startDate, endDate string, period Period
 			2 			High                     float64
 			3		 	Low                      float64
 			4 			Close                    float64
+			5 			Volume                   float6
