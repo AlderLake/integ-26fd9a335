548f4fbc626ec4dd89f399801780fbeffb78e857
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-07-10 16:34:42 +0000

    Update classic.html

--- quote.go
+++ quote.go
@@ -1278,4 +1278,4 @@ func NewQuoteFromBinance(symbol string, startDate, endDate string, period Period
 
 		for bar := 0; bar < numrows; bar++ {
 			q.Date[bar] = time.Unix(int64(bars[bar][6].(float64))/1000, 0)
-			q.Open[ba
+			q.Open[bar], _ = strconv.ParseFloat(bars[bar]
