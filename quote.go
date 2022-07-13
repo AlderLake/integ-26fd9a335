7a6428d93411c8684647daa1c374bbdffc4a55e6
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-07-13 20:37:41 +0000

    Back to CVS

--- quote.go
+++ quote.go
@@ -1300,4 +1300,4 @@ func NewQuoteFromBinance(symbol string, startDate, endDate string, period Period
 }
 
 // NewQuotesFromBinance - create a list of prices from symbols in file
-func NewQuotes
+func NewQuotesFromBinance(filename string, star
