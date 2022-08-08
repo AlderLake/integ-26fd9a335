b2e3ab595997e285f25f51a5eff804323cfd9eaf
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-08-08 20:44:51 +0000

    added google analytics tracking

--- quote.go
+++ quote.go
@@ -1442,4 +1442,4 @@ func NewMarketList(market string) ([]string, error) {
 	// case "nyse":
 	// 	url = "http://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download"
 	// case "megacap":
-	// 	url = "http://old.nasdaq.com/screening/companies-by-in
+	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?marketcap=Mega-cap&render=down
