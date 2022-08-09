4b07af771cbb02853a788f663521478034f37987
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-08-09 18:50:21 +0000

    fix #89

--- quote.go
+++ quote.go
@@ -1449,4 +1449,5 @@ func NewMarketList(market string) ([]string, error) {
 	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?marketcap=Mid-cap&render=download"
 	// case "smallcap":
 	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?marketcap=Small-cap&render=download"
-	// c
+	// case "microcap":
+	// 	url = "http://old.nasdaq.com
