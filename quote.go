b813ecbe28c89adcb9e0893fab0be3ffe4a8faae
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-08-17 14:45:24 +0000

    Update articles-archive.html

--- quote.go
+++ quote.go
@@ -1460,4 +1460,5 @@ func NewMarketList(market string) ([]string, error) {
 	// case "consumerdurables":
 	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Consumer%20Durables&render=download"
 	// case "consumernondurable":
-	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Consumer%20Non-Du
+	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Consumer%20Non-Durables&render=download"
+	// case "consumerse
