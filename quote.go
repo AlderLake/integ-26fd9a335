0e42ddf9542a98b2f6db04f9d3a747b4aa4a10c7
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-08-19 07:55:31 +0000

    update bindata

--- quote.go
+++ quote.go
@@ -1462,4 +1462,6 @@ func NewMarketList(market string) ([]string, error) {
 	// case "consumernondurable":
 	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Consumer%20Non-Durables&render=download"
 	// case "consumerservices":
-	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Consumer%20Services&render=download
+	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Consumer%20Services&render=download"
+	// case "energy":
+	// 	url = "ht
