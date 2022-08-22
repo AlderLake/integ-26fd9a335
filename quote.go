e4590844f1942a6b7b5b451b580bf40e1d547938
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-08-22 09:05:59 +0000

    updated tests.

--- quote.go
+++ quote.go
@@ -1468,4 +1468,4 @@ func NewMarketList(market string) ([]string, error) {
 	// case "finance":
 	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Finance&render=download"
 	// case "healthcare":
-	// 	url = "http://old.nasdaq.com/screening/com
+	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Health-Care&rende
