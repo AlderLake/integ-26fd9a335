fe174cd2e19952c07845827b19f25f76e106a33b
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-08-28 07:27:41 +0000

    py.testify another test.

--- quote.go
+++ quote.go
@@ -1478,4 +1478,4 @@ func NewMarketList(market string) ([]string, error) {
 	// case "transportation":
 	// 	url = "http://old.nasdaq.com/screening/companies-by-industry.aspx?industry=Transportation&render=download"
 	case "bittrex-btc":
-		url = "https://bittrex.co
+		url = "https://bittrex.com/Api/v2.0/pub/markets/getmarketsumma
