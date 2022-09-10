886a2f5dab47a86ccfb2d701705f480527c50f4a
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-09-10 13:25:48 +0000

    mockup

--- quote.go
+++ quote.go
@@ -1504,4 +1504,5 @@ func NewMarketList(market string) ([]string, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	req.Header.Add("User-Agent", "markcheno/go-quote")
 	req.Header.Add("Accept", "application/xml")
-	req.Header.Add("Content-Type", "application/xml; 
+	req.Header.Add("Content-Type", "application/xml; charset=utf-8")
+	client := &http.Clien
