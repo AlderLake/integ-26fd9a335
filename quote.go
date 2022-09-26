ea738b43d8613308c39255bdb1c21a0cedd805ac
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-09-26 15:53:46 +0000

    Bump version: 1.3.0 → 1.3.1

--- quote.go
+++ quote.go
@@ -1583,4 +1583,5 @@ func getBinanceMarket(market, rawdata string) ([]string, error) {
 	for _, mkt := range markets.Symbols {
 		if strings.HasSuffix(market, "bnb") && mkt.QuoteAsset == "BNB" {
 			symbols = append(symbols, mkt.Symbol)
-		} else if strings.HasSuffix(market, "btc") && mkt.Quo
+		} else if strings.HasSuffix(market, "btc") && mkt.QuoteAsset == "BTC" {
+			symbols = append(s
