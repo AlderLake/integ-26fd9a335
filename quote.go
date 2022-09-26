453347a1028450e4847d70938099a32298eba5ee
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-09-26 22:20:13 +0000

    fix misuses of f_count() in ppp and netlink

--- quote.go
+++ quote.go
@@ -1585,4 +1585,5 @@ func getBinanceMarket(market, rawdata string) ([]string, error) {
 			symbols = append(symbols, mkt.Symbol)
 		} else if strings.HasSuffix(market, "btc") && mkt.QuoteAsset == "BTC" {
 			symbols = append(symbols, mkt.Symbol)
-		} else if strings.HasSuffix(market, "eth") && mkt.Quo
+		} else if strings.HasSuffix(market, "eth") && mkt.QuoteAsset == "ETH" {
+			symbol
