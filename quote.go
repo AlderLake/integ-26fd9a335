48828093ec545283243207eea5b7f8a110766dcb
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-09-29 06:20:11 +0000

    Updated Norwegian bokmål translation. Same.

--- quote.go
+++ quote.go
@@ -1587,4 +1587,5 @@ func getBinanceMarket(market, rawdata string) ([]string, error) {
 			symbols = append(symbols, mkt.Symbol)
 		} else if strings.HasSuffix(market, "eth") && mkt.QuoteAsset == "ETH" {
 			symbols = append(symbols, mkt.Symbol)
-		} else if strings.HasSuffix(market, "usdt") && mkt.Q
+		} else if strings.HasSuffix(market, "usdt") && mkt.QuoteAsset == "USDT" {
+			symbols = appe
