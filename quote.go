a0967a0b8fa49743211aee82cf2ee637b218c81a
AlderLake <94998451+AlderLake@users.noreply.github.com>
2022-10-26 07:12:35 +0000

    b2

--- quote.go
+++ quote.go
@@ -1682,4 +1682,8 @@ func getBittrexMarket(market, rawdata string) ([]string, error) {
 		} else if strings.HasSuffix(market, "eth") && mkt.Market.BaseCurrency == "ETH" {
 			symbols = append(symbols, mkt.Market.MarketName)
 		} else if strings.HasSuffix(market, "usdt") && mkt.Market.BaseCurrency == "USDT" {
-			symbols = append(symbols, mkt.Market.Market
+			symbols = append(symbols, mkt.Market.MarketName)
+		}
+	}
+
+	return symbols, e
